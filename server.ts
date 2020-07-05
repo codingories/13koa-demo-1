e550fb8eb7b84d387cbc45e49b1f0a23c7e2fb37
朱骏杰 <ories.zhu@mochain.vip>
2020-07-05 21:01:15 +0800

    体验中间件执行顺序

--- server.ts
+++ server.ts
@@ -2,8 +2,22 @@ import Koa from 'koa';
 
 const app = new Koa();
 
-app.use(async (ctx) => {
+app.use(async (ctx, next) => {
   ctx.body = 'hello';
+  await next();
+  ctx.body += ' Eminem';
 });
+
+app.use(async (ctx, next) => {
+  ctx.body += ' world';
+  await next();
+});
+
+
+app.use(async (ctx, next) => {
+  ctx.set('Content-Type', 'text/html; charset=utf-8');
+  await next();
+});
+
 
 app.listen(3000);
