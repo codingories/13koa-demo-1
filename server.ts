9e606192b48869278f67312862c545b9d7c60042
朱骏杰 <ories.zhu@mochain.vip>
2020-07-05 21:09:05 +0800

    记录hello world耗时的案例

--- server.ts
+++ server.ts
@@ -2,21 +2,28 @@ import Koa from 'koa';
 
 const app = new Koa();
 
+// 记录开始到写完hello world整个的用时
 app.use(async (ctx, next) => {
-  ctx.body = 'hello';
+  // 空
   await next();
-  ctx.body += ' Eminem';
+  const time = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.url} - ${time}`);
 });
 
 app.use(async (ctx, next) => {
-  ctx.body += ' world';
+  const start = Date.now(); // 记录开始时间
   await next();
+  const time = Date.now() - start; // 记录结束时间 - start = 总耗时
+  ctx.set('X-Response-Time', `${time}ms`); // 写到response header里
 });
 
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+  for(let i=0; i<= 10000;i++){
+    ctx.body += ' Hello World'
+  }
 
-app.use(async (ctx, next) => {
-  ctx.set('Content-Type', 'text/html; charset=utf-8');
-  await next();
+  // 最后中间件可以不写 await next()
 });
 
 
